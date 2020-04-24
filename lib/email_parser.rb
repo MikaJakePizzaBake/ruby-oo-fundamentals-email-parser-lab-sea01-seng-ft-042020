2f09a298db4653fccd971dc1d967ae89a89b9fca
Trevor Low <tlow94@uw.edu>
2020-04-24 01:30:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,11 +6,11 @@ require 'pry'
 class EmailAddressParser
   attr_accessor :email
   def initialize (email)
-    binding.pry
     @email = []
-  end
-  def parse
     @email << email
+  end
+    binding.pry
+  def parse
     @email
   end
 end
